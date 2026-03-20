c2a8f428b2c899010fc9e52dfb3125770ad8b4b5
ajrp73 <161936563+ajrp73@users.noreply.github.com>
2026-03-20 13:07:27 +0000

    Fin de tuplas

---
+++ tuplas.py
@@ -0,0 +1,16 @@
+tupla1= (1, 2, 3, 'A', 'B', 'C')
+
+#Tienen el mismo comportamiento que las listas
+print("tupla1:", tupla1)
+print("tupla1[0]:",tupla1[0])
+print("tupla1Letras:", tupla1[3:])
+print("tupla1Letras:", tupla1[-1:-4:-1])
+
+#Las tuplas no permiten modificar los elementos
+#tupla1[0]= 10 Daría error
+
+#Tampoco permiten el borrado de elementos de la tupla
+#del(tupla1[0]) Daría error
+
+
+
